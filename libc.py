f32f5f47c5cc4d37b24102d230909552633e9ec5
lieanu <liuyue0310@gmail.com>
2016-04-21 23:04:52 +0800

    rewrite using libc-database repo

--- libc.py
+++ libc.py
@@ -1,133 +1,117 @@
 #!/usr/bin/env python2
 
 import os
+import re
 import struct
 import logging
 import logging.handlers
+import subprocess
 
 logging.basicConfig(format='%(asctime)s - %(filename)s:%(lineno)s - %(message)s',
                     level=logging.DEBUG)
 
-class libc(object):
-    def __init__(self, func, funcaddr):
-        self.func = func
-        if isinstance(funcaddr, (int, long)):
-            self.funcaddr = funcaddr
-        elif isinstance(funcaddr, basestring):
-            if funcaddr.startswith("0x"):
-                funcaddr = funcaddr[2:]
-            self.funcaddr = int(funcaddr, 16)
-        self.offset = self.funcaddr & 0xfff
-        self.dbpath = os.path.join(os.path.split(os.path.realpath(__file__))[0], "database")
-        self.all = self.__construct_dict()
+class LIBC(object):
+    def __init__(self, func=None, address=None):
+        if isinstance(address, basestring):
+            if address.startswith("0x"):
+                address = address[2:]
+            address = int(address, 16)
 
-    def __construct_dict(self):
-        all = {}
-        pfs = {8: 'B', 16: 'H', 32: 'I', 64: 'Q'}
+        self.condition = {}
+        if func:
+            self.condition[func] = address
 
-        for root, dirs, files in os.walk(self.dbpath):
-            for file in files:
-                fd = open(os.path.join(self.dbpath, file), "r")
-                mark = file.strip(".db")
-                all[mark] = []
-                for line in fd.readlines():
-                    if line.startswith("0"):
-                        line = line.strip("\n")
-                        linelist = line.split(" ")
-                        offset = struct.unpack(">" + pfs[len(linelist[0])*4], linelist[0].decode("hex"))[0]
-                        all[mark].append((offset, linelist[-1]))
-                fd.close()
+        self.libc_database_path = os.path.join(os.path.abspath(os.path.dirname("__file__")), "libc-database/db/")
 
-        return all
+    def add_condition(self, func, address):
+        if isinstance(address, basestring):
+            if address.startswith("0x"):
+                address = address[2:]
+            address = int(address, 16)
+        self.condition[func] = address
 
-    def __search(self):
+    def decided(self):
+        """Wrapper for libc-database's find shell script.
+        """
+        if len(self.condition) == 0:
+            logging.warning("No leaked info provided.\nPlease supply more info using \n\tadd_condition(leaked_func, leaked_address).")
+            exit(0)
+
+        res = []
+        for name, address in self.condition.items():
+            addr_last12 = address & 0xfff
+            res.append(re.compile("^%s .*%x" % (name, addr_last12)))
+
+        db = self.libc_database_path
+        files = []
+        for _, _, f in os.walk(db):
+            files += f
+
         result = []
-        for k, v in self.all.items():
-            for pair in v:
-                if (self.offset, self.func) == (pair[0]&0xfff, pair[1]):
-                    if (k, pair) not in result:
-                        result.append((k, pair))
+        for ff in files:
+            fd = open(db+ff, "r")
+            data = fd.read().split("\n")
+            flag = True
+            for x in res:
+                if any(map(lambda line:x.match(line), data)):
+                    result.append(ff)
+            fd.close()
 
-        if len(result) == 1:
-            return result[0]
-        elif len(result) == 0:
-            logging.warning("No match! try other libc.")
-            self.listall()
-            return (0, (0,0))
-        else:
-            print "[x]  Multi Results, Choose it manually, First Default:  " 
-            i = 0
-            for item in result:
-                print "     ID: ", i
-                print "     Version         : ", item[0]
-                print "     Function        : ", item[1][1]
-                print "     Address         : ", hex(item[1][0])
-                i += 1
+        if len(result) == 0:
+            logging.warning("No matched libc, try others.")
+            exit(0)
 
-            id = 0
+        if len(result) > 1:
+            print "Multi Results:"
+            for x in range(len(result)):
+                print "%2d: %s" %(x, self.pmore(result[x]))
+            print "Please supply more info using \n\tadd_condition(leaked_func, leaked_address)."
             while True:
+                in_id = input("You can choose it by hand\nOr type 'exit' to quit:")
+                if in_id == "exit" or in_id == "quit":
+                    exit(0)
                 try:
-                    id = int(raw_input("Input a Number [0]: "))
-                    if id < len(result):
-                        break
-                    else:
-                        logging.warning("Invalid ID")
-                except ValueError:
-                    break
-            return result[id]
+                    in_id = int(in_id)
+                    return result[in_id]
+                except:
+                    continue
+        return result[0]
 
-    def base(self):
-        (key, pair) = self.__search()
-        if key == 0:
-            return 0
-        return self.funcaddr - pair[0]
+    def pmore(self, result):
+        result = result[:-8] # .strip(".symbols")
+        fd = open(self.libc_database_path+result+".info")
+        info = fd.read().strip()
+        return "%s (id %s)" % (info, result)
 
-    def system_offset(self):
-        (key, pair) = self.__search()
-        if key == 0:
-            return 0
-        for one in self.all[key]:
-            if one[1] == "system":
-                return one[0]
+    def dump(self, func=None):
 
-    def system_address(self):
-        (key, pair) = self.__search()
-        if key == 0:
-            return 0
-        for one in self.all[key]:
-            if one[1] == "system":
-                return self.funcaddr - pair[0] + one[0]
+        db = self.decided()
+        db = self.libc_database_path + db
+        fd = open(db, "r")
+        data = fd.read().strip("\n").split("\n")
+        if not func:
+            result = {}
+            func = ["__libc_start_main_ret", "system", "dup2", "read", "write", "str_bin_sh"]
+            for ff in func:
+                for d in data:
+                    f= d.split(" ")[0]
+                    addr = d.split(" ")[1]
+                    if ff == f:
+                        result[ff] = int(addr, 16)
+            for k, v in result.items():
+                print k, hex(v)
+            return result
 
-    def offset_by_name(self, func):
-        (key, pair) = self.__search()
-        if key == 0:
-            return 0
-        for one in self.all[key]:
-            if one[1] == func:
-                return one[0]
+        for d in data:
+            f= d.split(" ")[0]
+            addr = d.split(" ")[1]
+            if func == f:
+                return int(addr, 16)
 
-    def address_by_name(self, func):
-        (key, pair) = self.__search()
-        if key == 0:
-            return 0
-        for one in self.all[key]:
-            if one[1] == func:
-                return self.funcaddr - pair[0] + one[0]
-
-
-    def info(self):
-        (key, pair) = self.__search()
-        return key
-
-    def listall(self):
-        print "All available libc database : "
-        i = 0
-        for k, v in self.all.items():
-            i += 1
-            print str(i) + ":\t" + k
+        logging.warning("No matched, Make sure you supply a valid function name.")
+        return 0
 
 if __name__ == "__main__" :
     obj = libc("fgets", "7ff39014bd90")
-    print "[+]system address: ", hex(obj.system_address())
-    print "[+]libc info     : ", obj.info()
-    obj.listall()
+    print "[+]system  offset: ", hex(obj.dump("system"))
+    print "[+]/bin/sh offset: ", hex(obj.dump("str_bin_sh"))
